4ce85d7fcd0f38624809fec066e69949c18dcf8a
Daniil Trotsenko <s.danik.tro.c@gmail.com>
2019-11-20 19:53:21 +0200

    Sorted cards in class

--- FlluentPython.py
+++ FlluentPython.py
@@ -19,6 +19,22 @@ def colods_of_card():
 
     from random import choice
     print(choice(deck))
+    print(choice(deck))
+    print(choice(deck))
 
+    print(deck[:13])
+    print(deck[12::13])
+
+    for card in deck:
+        print(card)
+
+    suit_values = dict(spades=3, hearts=2, diamonds=1, clubs=0)
+
+    def spades_high(card):
+        rank_value = FrenchDeck.ranks.index(card.rank)
+        return rank_value * len(suit_values) + suit_values[card.suit]
+
+    for card in sorted(deck, key=spades_high):
+        print(card)
 
 colods_of_card()
