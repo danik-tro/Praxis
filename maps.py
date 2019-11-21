ae3be608f6a4fe8fd3c6cff4a89d7f578adba881
Daniil Trotsenko <s.danik.tro.c@gmail.com>
2019-11-21 21:29:47 +0200

    Final version maps

--- maps.py
+++ maps.py
@@ -1,9 +1,11 @@
+"""
+!folium.Marker(location=[37.4074687,-122.086669], popup = "Google HQ", icon=folium.Icon(color = 'gray')).add_to(maps) add marker
+"""
+
 import folium
 import pandas as pd
 from folium.plugins import MarkerCluster
-
-
-data = pd.read_csv("maps/data/Volcanoes_USA.txt")
+import sys
 
 
 def color_change(evl):
@@ -14,18 +16,19 @@ def color_change(evl):
     else:
         return 'red' 
 
-lat = data['LAT']
-lon = data['LON']
-elevation = data['ELEV']
-name = data['NAME']
+if __name__ == "__main__":
+    data = pd.read_csv("maps/data/Volcanoes_USA.txt")
 
-maps = folium.Map(location=[50.4546600,30.5238000], zoom_start=8)
+    lat = data['LAT']
+    lon = data['LON']
+    elevation = data['ELEV']
+    name = data['NAME']
 
-"""
-!folium.Marker(location=[37.4074687,-122.086669], popup = "Google HQ", icon=folium.Icon(color = 'gray')).add_to(maps) add marker
-"""
+    maps = folium.Map(location=[37.4074687,-122.086669], zoom_start=5, tiles= 'CartoDB dark_matter')
 
-for lat_, lon_, elv_, name_ in zip(lat, lon, elevation, name):
-    folium.CircleMarker(location=[lat_, lon_],radius=9,  popup=(name_ + "\n" + str(elv_) + " m"), fill_color = color_change(elv_), color="gray", fill_opacity = 0.9).add_to(maps)
 
-maps.save('maps/maps1.html')
+    marker_cluster = MarkerCluster().add_to(maps)
+    for lat_, lon_, elv_, name_ in zip(lat, lon, elevation, name):
+        folium.CircleMarker(location=[lat_, lon_],radius=9,  popup=(name_ + "\n" + str(elv_) + " m"), fill_color = color_change(elv_), color="gray", fill_opacity = 0.9).add_to(marker_cluster)
+
+    maps.save('maps/maps1.html')
