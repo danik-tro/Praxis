79425e00ef7ff199f309d8de0c5c2c20a44f1d19
Daniil Trotsenko <s.danik.tro.c@gmail.com>
2019-11-28 00:30:27 +0200

    Create own dict with methods missing

--- FlluentPython.py
+++ FlluentPython.py
@@ -306,6 +306,46 @@ def deque_():
     #90 page of book fluent python
     #string and bytes
 
+def hash_():
+    x = (1,2, (30, 40))
+    print(hash(x))
+    y = (1, 2, [30, 40])
+
+    try:
+        print(hash(y))
+    except TypeError:
+        print('Error,',y,'is not hashable')
+    
+    a = dict(one=1, two=2, three=3)
+    b = {'one': 1, 'two': 2, 'three': 3}
+    c = dict((zip(['one', 'two', 'three'], (1, 2, 3))))
+    d = dict([('two', 2), ('one', 1), ('three', 3)])
+    e = dict({'three': 3, 'one': 1, 'two': 2})
+    print(a == b == c == d == e)
+
+def dict_and_miss():
+
+    class StrKeyDict0(dict):
+
+        def __missing__(self, key):
+            if isinstance(key, str):
+                raise KeyError(key)
+            return self[str(key)]
+        
+        def get(self, key, default=None):
+            try:
+                return self[key]
+            except KeyError:
+                return default
+
+        def __contains__(self, key):
+            return key in self.keys() or str(key) in self.keys()
+
+    x = StrKeyDict0([(1,'e'), (2,'d')])
+    print(x.get(3))
+dict_and_miss()
+
+
 
 
 
